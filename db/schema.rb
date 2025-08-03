6505c2d0f1cc6112745f06e9437e65265a1c3003
EC2 Default User <ec2-user@ip-172-31-38-20.ap-northeast-1.compute.internal>
2025-08-03 05:31:54 +0000

    [Add]delete

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,7 @@ ActiveRecord::Schema.define(version: 2025_08_01_012915) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "post_images", id: false, force: :cascade do |t|
-    t.integer "id"
+  create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
